beb6ecdc621479eacbc8d78d185bb819160dd731
Radisa <radisa@gmail.com>
2018-02-14 14:31:31 +0100

    added set -ex

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -ex
+
 source ./conf.sh
 
-docker run -ti -P --rm $@ ${NAMESPACE}/${REPOSITORY}:${TAG}
+docker run -ti -p 10080:80 -p 10443:443 -v "${PWD}/html/:/var/www/html/" --rm --name ${REPOSITORY} $@ ${NAMESPACE}/${REPOSITORY}:${TAG}
+
