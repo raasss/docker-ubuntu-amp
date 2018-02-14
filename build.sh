7fa4264c47c94c3912104c672535a33fa3ac9b8b
Radisa <radisa@gmail.com>
2018-02-14 14:30:55 +0100

    added set -ex

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 source ./conf.sh
 
 docker build -t ${NAMESPACE}/${REPOSITORY}:${TAG} .
--- exec.sh
+++ exec.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 source ./conf.sh
 
 docker run -ti -P --rm $@ ${NAMESPACE}/${REPOSITORY}:${TAG} /bin/bash
